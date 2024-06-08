8a9bd7f96011499207834272797ead3025566fbf
Kumar Parajuli <kumar.parajuli@imark.com.np>
2024-06-08 15:38:22 +0545

    Creating splashScreen ,StartActivity,& starting LoginActivity design

---
+++ app/src/main/java/com/example/foodorderingapplication/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.foodorderingapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.ActionMode;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, StartActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
