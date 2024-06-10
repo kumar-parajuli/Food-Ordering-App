317a44ccacef91159f026bc712ff59198b0325ea
Kumar Parajuli <kumar.parajuli@imark.com.np>
2024-06-10 11:36:44 +0545

    adding the start activity UI & design the UI for Login page

--- app/src/main/java/com/example/foodorderingapplication/StartActivity.java
+++ app/src/main/java/com/example/foodorderingapplication/StartActivity.java
@@ -1,6 +1,9 @@
 package com.example.foodorderingapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class StartActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_start);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        // Bind the button
+        Button redirectToLoginButton = findViewById(R.id.nextbutton);
+
+        // Set up the click listener
+        redirectToLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create an Intent to start LoginActivity
+                Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
         });
+
     }
 }
