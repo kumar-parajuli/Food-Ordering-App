e802eb38ee59ff74bfa9175ca13c69d24730fdbc
Kumar Parajuli <kumar.parajuli@imark.com.np>
2024-07-19 14:22:31 +0545

    Complete signup Ui with redirect activity

--- app/src/main/java/com/example/foodorderingapplication/LoginActivity.java
+++ app/src/main/java/com/example/foodorderingapplication/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.foodorderingapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,5 +20,19 @@ public class LoginActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
+        // Bind the TextView
+        TextView redirectToSignupTextView = findViewById(R.id.nobutton);
+
+        // Set up the click listener
+        redirectToSignupTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create an Intent to start LoginActivity
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
