2a796ee2c3505360429f5f624391e5e427794be2
Kumar Parajuli <kumar.parajuli@imark.com.np>
2024-07-20 23:23:57 +0545

    adding the imageslider on project and add the related image banner and some logic to populate the image slide on project

--- app/src/main/java/com/example/foodorderingapplication/Fragement/HomeFragment.java
+++ app/src/main/java/com/example/foodorderingapplication/Fragement/HomeFragment.java
@@ -2,13 +2,21 @@ package com.example.foodorderingapplication.Fragement;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.foodorderingapplication.R;
+import com.example.foodorderingapplication.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +33,7 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private FragmentHomeBinding binding;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -61,6 +70,20 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        binding=FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ArrayList<SlideModel> imageList = new ArrayList<>();
+        imageList.add(new SlideModel(R.drawable.banner1, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.banner2, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.banner3, ScaleTypes.FIT));
+
+        ImageSlider imageSlider = binding.imageSlider;
+        imageSlider.setImageList(imageList);
+        imageSlider.setImageList(imageList, ScaleTypes.FIT);
     }
 }
